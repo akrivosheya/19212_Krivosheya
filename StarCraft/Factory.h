4c676f5bc4f311d21c565e00223b4dd28d4d7af2
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2020-10-07 10:22:34 +0700

    Update Factory.h

--- StarCraft/Factory.h
+++ StarCraft/Factory.h
@@ -4,10 +4,17 @@ typedef Unit* (*creator_t)();
 
 class Factory {
 public:
+	
 	Unit* makeUnit(const std::string& name);
 
 	void addCreator(const std::string& name, creator_t c);
+	
+	static Factory* getInstance();
 
 private:
+	Factory();
+	~Factory();
+	Factory(const Factory&);
+	Factory& operator=(const Factory&);
 	HashTable creators_;//???????????????
 };
