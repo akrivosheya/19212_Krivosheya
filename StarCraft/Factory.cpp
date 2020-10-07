5a171d1a397dec952c3742cc89fed3d2df19dfba
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2020-10-07 10:22:39 +0700

    Update Factory.cpp

--- StarCraft/Factory.cpp
+++ StarCraft/Factory.cpp
@@ -1,5 +1,13 @@
 #include "Factory.h"
 
+Factory::Factory() = default;
+Factory::~Factory() = default;
+
+Factory* Factory::getInstance(){
+	static Factory f;
+	return &f;
+}
+
 Unit* Factory::makeUnit(const std::string& name) {
 	creator_t c = creators_.at(name);
 	return c();
