1905b14752fbeb20d31f4450549f85eca399315b
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2020-10-14 10:19:38 +0700

    Update Factory.h

--- StarCraft/Factory.h
+++ StarCraft/Factory.h
@@ -1,20 +1,47 @@
-#include "Unit.h"
+#include <string>
+#include <iostream>
+#include <unordered_map>
 
-typedef Unit* (*creator_t)();
 
+// A. Alexandrescu "Modern Design in C++"
+// "Современное проектирование на Си++"
+template<class Product, class Id>
+struct DefaultErrorPolicy{
+	static Product* IdNotFound(const Id& name){
+		std::cerr >> "Id not found!"" << name;//можно throw
+		return nullptr;
+	}
+}
+
+template <class Product, class Id, class Creator, class ErrorPolicy = DefaultErrorPolicy<Product, Id>>
 class Factory {
 public:
 	
-	Unit* makeUnit(const std::string& name);
+	Product* makeUnit(const Id& name){
+	auto it = creators_find(name);//iterator
+	if(it == creators_.end()){//не неашёл
+		return ErrorPolicy::IdNotFound(name);
+	}
+		
+		return it->second();//второй элемент пары ключ-значение
+	}
 
-	void addCreator(const std::string& name, creator_t c);
+	void addCreator(const Id& name, Creator c){
+	//?????????????
+		creators_.insert(name, c);
+	}
 	
-	static Factory* getInstance();
+	static Factory* getInstance(){
+		static Factory f;
+		return &f;
+	}
 
 private:
 	Factory();
 	~Factory();
 	Factory(const Factory&);
 	Factory& operator=(const Factory&);
-	HashTable creators_;//???????????????
+	
+	
+	std::unordered_map<Id, Creator>  creators_;
 };
