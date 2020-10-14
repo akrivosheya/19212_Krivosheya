ed529dabc53fe3e198ab91539ccbd81f19d1ee61
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2020-10-14 10:20:09 +0700

    Update Main.cpp

--- StarCraft/Main.cpp
+++ StarCraft/Main.cpp
@@ -3,5 +3,5 @@
 int main(int argc, char* argv[]){
   if (argc < 2) return -1;
   
-  std::unique_ptr<Unit> u (Factory::getInstance->makeUnit(argv[1]));//смарт поинтер
+  std::unique_ptr<Unit> u (Factory<Unit, std::string, Unit* (*)()>::getInstance->makeUnit(argv[1]));//смарт поинтер
 }
