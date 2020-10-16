2e8f70289c5b285f355f14392d81e3741c99a21b
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2020-10-16 22:23:32 +0700

    Create TitForTatAnger.cpp

---
+++ lab2/TitForTatAnger.cpp
@@ -0,0 +1,22 @@
+#include "TitForTatAnger.h"
+#include "Factory.h"
+
+namespace {
+	bool g() {
+		Factory<Strategy, std::string, Strategy* (*)()>::getInstance()->addCreator("titfortatanger", createTitForTatAnger);
+		return true;//у каждого класса
+	}
+}
+
+void TitForTatAnger::getRes(bool s1, bool s2) {
+	betrayed = s1 & s2;
+	return;
+}
+
+bool TitForTatAnger::makeMove() {
+	return betrayed;
+}
+
+Strategy* createTitForTatAnger() {
+	return new TitForTatAnger;
+}
