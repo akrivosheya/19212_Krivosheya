a1e79fadb1ee450af413bd90a59f7a65fd147a10
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2020-10-16 22:25:45 +0700

    Create BackAndForth.cpp

---
+++ lab2/BackAndForth.cpp
@@ -0,0 +1,22 @@
+#include "BackAndForth.h"
+#include "Factory.h"
+
+namespace {
+	bool g() {
+		Factory<Strategy, std::string, Strategy* (*)()>::getInstance()->addCreator("backandforth", createBackAndForth);
+		return true;//у каждого класса
+	}
+}
+
+void BackAndForth::getRes(bool s1, bool s2) {
+	return;
+}
+
+bool BackAndForth::makeMove() {
+	cORd = !cORd;
+	return cORd;
+}
+
+Strategy* createBackAndForth() {
+	return new BackAndForth;
+}
