8f5fa64cccb48824cb709d586a15ea1042380e4b
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2020-10-16 22:22:25 +0700

    Create TitForTatKind.cpp

---
+++ lab2/TitForTatKind.cpp
@@ -0,0 +1,22 @@
+#include "TitForTatKind.h"
+#include "Factory.h"
+
+namespace {
+	bool g() {
+		Factory<Strategy, std::string, Strategy* (*)()>::getInstance()->addCreator("titfortatkind", createTitForTatKind);
+		return true;//у каждого класса
+	}
+}
+
+void TitForTatKind::getRes(bool s1, bool s2) {
+	betrayed = s1 | s2;
+	return;
+}
+
+bool TitForTatKind::makeMove() {
+	return betrayed;
+}
+
+Strategy* createTitForTatKind() {
+	return new TitForTatKind;
+}
