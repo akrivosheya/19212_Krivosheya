eed4310f5edbf05e53d7060e4c9c2b4a8665c1eb
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2020-10-20 22:45:55 +0700

    Fixed some things

--- Lab1/test.cpp
+++ Lab1/test.cpp
@@ -4,14 +4,15 @@
 
 TEST(TestCaseName, ConstrDefault) {
 	HashTable A;
-	EXPECT_TRUE(A.size() == 8);
+	EXPECT_EQ(A.size(), 8);
 }
 
 TEST(TestCaseName, InsertAndOperAndAt)
 {
 	HashTable A;
 	A.insert("Bill", { 19, 83 });
-	EXPECT_TRUE((A["Bill"].age == 19) && (A.at("Bill").weight == 83));
+	EXPECT_EQ(A["Bill"].age, 19);
+	EXPECT_EQ(A.at("Bill").weight, 83);
 }
 
 TEST(TestCaseName, ConstrParam)
@@ -20,8 +21,10 @@ TEST(TestCaseName, ConstrParam)
 	A.insert("Bill", { 19, 83 });
 	A.insert("John", { 20, 70 });
 	HashTable B(A);
-	EXPECT_TRUE((B["Bill"].age == 19) && (B["Bill"].weight == 83) && \
-		(B["John"].age == 20) && (B["John"].weight == 70));
+	EXPECT_EQ(B["Bill"].age, 19);
+	EXPECT_EQ(B["Bill"].weight, 83);
+	EXPECT_EQ(B["John"].age, 20);
+	EXPECT_EQ(B["John"].weight, 70);
 }
 
 TEST(TestCaseName, ConstrParamAndChanged)
@@ -30,7 +33,8 @@ TEST(TestCaseName, ConstrParamAndChanged)
 	A.insert("Bill", { 19, 83 });
 	HashTable B(A);
 	B["Bill"].age = 20;
-	EXPECT_TRUE((A["Bill"].age == 19) && (B["Bill"].age == 20));
+	EXPECT_EQ(A["Bill"].age, 19);
+	EXPECT_EQ(B["Bill"].age, 20);
 }
 
 TEST(TestCaseName, OperatorAndContains)
@@ -60,7 +64,7 @@ TEST(TestCaseName, OperWithEmpty)
 {
 	HashTable A;
 	A["Bill"].age = 24;
-	EXPECT_TRUE(A.at("Bill").age == 24);
+	EXPECT_EQ(A.at("Bill").age, 24);
 }
 
 TEST(TestCaseName, ConstAt)
@@ -68,7 +72,7 @@ TEST(TestCaseName, ConstAt)
 	HashTable A;
 	A.insert("John", { 19, 76 });
 	const HashTable B = A;
-	EXPECT_TRUE(B.at("John").weight == 76);
+	EXPECT_EQ(B.at("John").weight, 76);
 }
 
 TEST(TestCaseName, Swap)
@@ -80,8 +84,11 @@ TEST(TestCaseName, Swap)
 	B.insert("Andrew", { 20, 83 });
 	B.insert("Finn", { 19, 80 });
 	A.swap(B);
-	EXPECT_TRUE(B.contains("Bill") && B.contains("John") && B.contains("Ann") && \
-		A.contains("Andrew") && A.contains("Finn"));
+	EXPECT_TRUE(B.contains("Bill"));
+	EXPECT_TRUE(B.contains("John"));
+	EXPECT_TRUE(B.contains("Ann"));
+	EXPECT_TRUE(A.contains("Andrew"));
+	EXPECT_TRUE(A.contains("Finn"));
 }
 
 TEST(TestCaseName, Erase)
@@ -91,7 +98,15 @@ TEST(TestCaseName, Erase)
 	A.insert("Bill", { 20, 75 });
 	A.insert("Ann", { 18, 69 });
 	A.erase("Bill");
-	EXPECT_TRUE(A.contains("John") && A.contains("Ann") && !A.contains("Bill"));
+	EXPECT_TRUE(A.contains("John"));
+	EXPECT_TRUE(A.contains("Ann"));
+	EXPECT_TRUE(!A.contains("Bill"));
+}
+
+TEST(TestCaseName, EraseNothing)
+{
+	HashTable A;
+	EXPECT_TRUE(!A.erase("Bill"));
 }
 
 TEST(TestCaseName, Empty)
@@ -100,7 +115,8 @@ TEST(TestCaseName, Empty)
 	A.insert("John", { 18, 70 });
 	A.insert("Bill", { 20, 75 });
 	HashTable B;
-	EXPECT_TRUE(B.empty() && !A.empty());
+	EXPECT_TRUE(B.empty());
+	EXPECT_TRUE(!A.empty());
 }
 
 TEST(TestCaseName, Equal1)
@@ -195,38 +211,166 @@ TEST(TestCaseName, IncreaseTable)
 	A.insert("Shrek", { 19, 78 });
 	A.insert("Hayns", { 19, 80 });
 	A.insert("Dendy", { 21, 76 });
-	EXPECT_TRUE(A.size() == 16);
+	EXPECT_EQ(A.size(), 16);
+	EXPECT_TRUE(A.contains("John"));
+	EXPECT_TRUE(A.contains("Bill"));
+	EXPECT_TRUE(A.contains("Wendy"));
+	EXPECT_TRUE(A.contains("Dean"));
+	EXPECT_TRUE(A.contains("Alastor"));
+	EXPECT_TRUE(A.contains("Freddy"));
+	EXPECT_TRUE(A.contains("Harry"));
+	EXPECT_TRUE(A.contains("Alukard"));
+	EXPECT_TRUE(A.contains("Aden"));
+	EXPECT_TRUE(A.contains("Shrek"));
+	EXPECT_TRUE(A.contains("Hayns"));
+	EXPECT_TRUE(A.contains("Dendy"));
+}
+
+TEST(TestCaseName, CopyBigTable)
+{
+	HashTable A;
+	A.insert("John", { 18, 70 });
+	A.insert("Bill", { 19, 75 });
+	A.insert("Wendy", { 19, 80 });
+	A.insert("Dean", { 20, 81 });
+	A.insert("Alastor", { 19, 82 });
+	A.insert("Freddy", { 18, 83 });
+	A.insert("Harry", { 21, 76 });
+	A.insert("Alukard", { 26, 90 });
+	A.insert("Aden", { 20, 81 });
+	A.insert("Shrek", { 19, 78 });
+	A.insert("Hayns", { 19, 80 });
+	A.insert("Dendy", { 21, 76 });
+	HashTable B = A;
+	EXPECT_TRUE(B.contains("John"));
+	EXPECT_TRUE(B.contains("Bill"));
+	EXPECT_TRUE(B.contains("Wendy"));
+	EXPECT_TRUE(B.contains("Dean"));
+	EXPECT_TRUE(B.contains("Alastor"));
+	EXPECT_TRUE(B.contains("Freddy"));
+	EXPECT_TRUE(B.contains("Harry"));
+	EXPECT_TRUE(B.contains("Alukard"));
+	EXPECT_TRUE(B.contains("Aden"));
+	EXPECT_TRUE(B.contains("Shrek"));
+	EXPECT_TRUE(B.contains("Hayns"));
+	EXPECT_TRUE(B.contains("Dendy"));
+	EXPECT_TRUE(A == B);
+}
+
+TEST(TestCaseName, CompareBigTables)
+{
+	HashTable A;
+	A.insert("John", { 18, 70 });
+	A.insert("Bill", { 19, 75 });
+	A.insert("Wendy", { 19, 80 });
+	A.insert("Dean", { 20, 81 });
+	A.insert("Alastor", { 19, 82 });
+	A.insert("Freddy", { 18, 83 });
+	A.insert("Harry", { 21, 76 });
+	A.insert("Alukard", { 26, 90 });
+	A.insert("Aden", { 20, 81 });
+	A.insert("Shrek", { 19, 78 });
+	A.insert("Hayns", { 19, 80 });
+	A.insert("Dendy", { 21, 76 });
+	HashTable B;
+	B.insert("John", { 18, 70 });
+	B.insert("Bill", { 19, 75 });
+	B.insert("Wendy", { 19, 80 });
+	B.insert("Dean", { 20, 81 });
+	B.insert("Alastor", { 19, 82 });
+	B.insert("Freddy", { 18, 83 });
+	B.insert("Harry", { 21, 76 });
+	B.insert("Alukard", { 26, 90 });
+	B.insert("Aden", { 20, 81 });
+	B.insert("Shrek", { 19, 78 });
+	B.insert("Hayns", { 19, 80 });
+	B.insert("Dendy", { 21, 76 });
+	EXPECT_TRUE(A == B);
+}
+
+TEST(TestCaseName, EraseChain)
+{
+	HashTable A;
+	A.insert("John", { 18, 70 });
+	A.insert("Bill", { 19, 75 });
+	A.insert("Wendy", { 19, 80 });
+	A.insert("Dean", { 20, 81 });
+	A.insert("Alastor", { 19, 82 });
+	A.insert("Freddy", { 18, 83 });
+	A.insert("Harry", { 21, 76 });
+	A.insert("Alukard", { 26, 90 });
+	A.insert("Aden", { 20, 81 });
+	A.insert("Shrek", { 19, 78 });
+	A.insert("Hayns", { 19, 80 });
+	A.insert("Dendy", { 21, 76 });
+	A.erase("Dendy");
+	A.erase("Hayns");
+	A.erase("Shrek");
+	A.erase("Aden");
+	A.erase("Alukard");
+	A.erase("Harry");
+	A.erase("Freddy");
+	A.erase("Alastor");
+	A.erase("Dean");
+	A.erase("Wendy");
+	A.erase("Bill");
+	A.erase("John");
+	EXPECT_EQ(A.size(), 16);
+	EXPECT_TRUE(A.empty());
+}
+
+TEST(TestCaseName, GetAmongMany)
+{
+	HashTable A;
+	A.insert("John", { 18, 70 });
+	A.insert("Bill", { 19, 75 });
+	A.insert("Wendy", { 19, 80 });
+	A.insert("Dean", { 20, 81 });
+	A.insert("Alastor", { 19, 82 });
+	A.insert("Freddy", { 18, 83 });
+	A.insert("Harry", { 21, 76 });
+	A.insert("Alukard", { 26, 90 });
+	A.insert("Aden", { 20, 81 });
+	A.insert("Shrek", { 19, 78 });
+	A.insert("Hayns", { 19, 80 });
+	A.insert("Dendy", { 21, 76 });
+	EXPECT_EQ(A.at("Dendy").weight, 76);
+}
+
+TEST(TestCaseName, GetConstAmongMany)
+{
+	HashTable A;
+	A.insert("John", { 18, 70 });
+	A.insert("Bill", { 19, 75 });
+	A.insert("Wendy", { 19, 80 });
+	A.insert("Dean", { 20, 81 });
+	A.insert("Alastor", { 19, 82 });
+	A.insert("Freddy", { 18, 83 });
+	A.insert("Harry", { 21, 76 });
+	A.insert("Alukard", { 26, 90 });
+	A.insert("Aden", { 20, 81 });
+	A.insert("Shrek", { 19, 78 });
+	A.insert("Hayns", { 19, 80 });
+	A.insert("Dendy", { 21, 76 });
+	const HashTable B = A;
+	EXPECT_EQ(B.at("Dendy").weight, 76);
+	EXPECT_EQ(B.size(), 16);
 }
 
 TEST(TestCaseName, Exception)
 {
 	HashTable A;
-	int error = 0;
-	try {
-		A.at("Anybody");
-	}
-	catch (int) {
-		error = 1;
-	}
-	EXPECT_TRUE(error == 1);
+	EXPECT_ANY_THROW(A.at("Anybody"));
 }
 
 TEST(TestCaseName, ExceptionConst)
 {
-	HashTable A;
-	int error = 0;
-	try {
-		const Value b = A.at("Anybody");
-	}
-	catch (int) {
-		error = 1;
-	}
-	EXPECT_TRUE(error == 1);
+	const HashTable A;
+	EXPECT_ANY_THROW(A.at("Anybody"));
 }
 
 int main(int argc, char** argv) {
 	::testing::InitGoogleTest(&argc, argv);
 	int j, i = RUN_ALL_TESTS();
-	std::cin >> j;
 	return i;
 }
