5082044d21dae8bca7085f0d8a36d026292379db
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2020-10-30 23:21:09 +0700

    Update Kind.cpp

--- lab2/Kind.cpp
+++ lab2/Kind.cpp
@@ -6,6 +6,8 @@ namespace{
 		Factory<Strategy, std::string, Strategy* (*)()>::getInstance()->addCreator("kind", createKind);
 		return true;//у каждого класса
 	}
+	
+	bool b = g();
 }
 
 void Kind::getRes(bool s1, bool s2) {
