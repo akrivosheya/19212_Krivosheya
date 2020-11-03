c438989bd155305c2bebc54997e68b22eb0a75c3
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2020-11-03 22:42:36 +0700

    Update BackAndForth.h

--- lab2/BackAndForth.h
+++ lab2/BackAndForth.h
@@ -4,8 +4,11 @@ class BackAndForth : public Strategy {
 public:
 	void getRes(bool s1, bool s2) override;
 	bool makeMove() override;
+	std::string getName() override;
+	void reload() override;
 private:
 	bool cORd = c;
+	std::string name = "BackAndForth";
 };
 
 Strategy* createBackAndForth();
