9b3c56b1be470c4bf30bf31a8a12f357fa36a801
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2020-11-03 22:43:27 +0700

    Update Kind.h

--- lab2/Kind.h
+++ lab2/Kind.h
@@ -4,6 +4,10 @@ class Kind : public Strategy {
 public:
 	void getRes(bool s1, bool s2) override;
 	bool makeMove() override;
+	std::string getName() override;
+	void reload() override {};
+private:
+	std::string name = "Kind";
 };
 
 Strategy* createKind();
