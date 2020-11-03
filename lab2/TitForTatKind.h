cb89ec93feb79d37a61e3e6ea4a633bda77ed65e
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2020-11-03 22:46:49 +0700

    Update TitForTatKind.h

--- lab2/TitForTatKind.h
+++ lab2/TitForTatKind.h
@@ -4,8 +4,11 @@ class TitForTatKind : public Strategy {
 public:
 	void getRes(bool s1, bool s2) override;
 	bool makeMove() override;
+	std::string getName() override;
+	void reload() override;
 private:
 	bool betrayed = false;
+	std::string name = "TitForTatKind";
 };
 
 Strategy* createTitForTatKind();
