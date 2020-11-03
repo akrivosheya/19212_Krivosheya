684b0021430ecab12950b97b1e249e300366b6eb
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2020-11-03 22:45:12 +0700

    Update Strategy.h

--- lab2/Strategy.h
+++ lab2/Strategy.h
@@ -6,7 +6,9 @@ public:
 	virtual ~Strategy() {}
 	virtual void getRes(bool s1, bool s2) = 0;
 	virtual bool makeMove() = 0;
+	virtual std::string getName() = 0;
+	virtual void reload() = 0;
 protected:
 	static constexpr bool c = true;
-	static constexpr bool d = true;
+	static constexpr bool d = false;
 };
