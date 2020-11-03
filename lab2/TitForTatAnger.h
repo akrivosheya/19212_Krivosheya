4494f04e49d43eefeaffe64ba7fb24481d00aa71
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2020-11-03 22:46:00 +0700

    Update TitForTatAnger.h

--- lab2/TitForTatAnger.h
+++ lab2/TitForTatAnger.h
@@ -4,8 +4,11 @@ class TitForTatAnger : public Strategy {
 public:
 	void getRes(bool s1, bool s2) override;
 	bool makeMove() override;
+	std::string getName() override;
+	void reload() override;
 private:
 	bool betrayed = false;
+	std::string name = "TitForTatAnger";
 };
 
 Strategy* createTitForTatAnger();
