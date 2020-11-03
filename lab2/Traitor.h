04b8b34f6f8890e0a0c04ced1f0cf12b566810de
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2020-11-03 22:47:35 +0700

    Update Traitor.h

--- lab2/Traitor.h
+++ lab2/Traitor.h
@@ -4,6 +4,10 @@ class Traitor : public Strategy {
 public:
 	void getRes(bool s1, bool s2) override;
 	bool makeMove() override;
+	std::string getName() override;
+	void reload() override {};
+private:
+	std::string name = "Traitor";
 };
 
 Strategy* createTraitor();
