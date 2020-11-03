b8c92d6d24dcad2db0bff58bd45bddd315f7a676
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2020-11-03 22:44:52 +0700

    Update Southampton.h

--- lab2/Southampton.h
+++ lab2/Southampton.h
@@ -2,25 +2,20 @@
 
 class Southampton : public Strategy {
 public:
-	Southampton();/* : counter(0), winner(true), s1(true), s2(true) {
-		if (winnerIs) {
-			winner = false;
-			return;
-		}
-		winnerIs = true;
-	}*/
+	Southampton();
 	void getRes(bool s1, bool s2) override;
 	bool makeMove() override;
+	std::string getName() override;
+	void reload() override;
 private:
 	static constexpr int dancing = 618;
 	static constexpr int mask = 1;
 	static bool winnerIs;
+	std::string name = "Southampton";
 	int counter;
 	bool winner;
 	bool s1;
 	bool s2;
 };
-
-//bool Southampton::winnerIs = false;
 
 Strategy* createSouthampton();
