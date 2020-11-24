7d2f5281453bf7e822e8b38ffd9df8b4317a3ec7
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2020-11-24 22:16:12 +0700

    Now it is header for class Game

--- lab2/Game.h
+++ lab2/Game.h
@@ -1,30 +1,23 @@
 #pragma once
 #include <string>
 #include "Strategy.h"
+#include <vector>
+#include <fstream>
 
-
-#define OPTIONALSTEPS 100
-
-bool IsDigit(std::string str);
-
-bool IsMode(char* str);
-
-void SetModeOptional(const int& size, std::string& mode);
-
-bool SetOptions(const int& size, std::string& mode, int& steps, int& i, const int& argc, char* argv[]);
-
-std::string Moves(bool& moveS1, bool& moveS2, bool& moveS3);
-
-void GetMoves(bool& moveS1, bool& moveS2, bool& moveS3, Strategy* S1, Strategy* S2, Strategy* S3);
-
-void GetGets(bool& moveS1, bool& moveS2, bool& moveS3, int& getS1, int& getS2, int& getS3);
-
-void GetPoints(int& pointS1, int& pointS2, int& pointS3, int& getS1, int& getS2, int& getS3);
-
-void PutRes(bool& moveS1, bool& moveS2, bool& moveS3, Strategy* S1, Strategy* S2, Strategy* S3);
-
-void PrintRes(int& pointS1, int& pointS2, int& pointS3, Strategy* S1, Strategy* S2, Strategy* S3);
-
-void Reload(Strategy* S1, Strategy* S2, Strategy* S3);
-
-void SetWinners(int& pointS1, int& pointS2, int& pointS3, int& S1, int& S2, int& S3);
+class Game {
+public:
+	virtual void Play(int& steps, std::vector<Strategy*>& strategys, const char* file) = 0;
+	int GetPoint(int i);
+	virtual ~Game() {}
+protected:
+	std::string Moves(bool& moveS1, bool& moveS2, bool& moveS3);
+	void GetMoves(bool& moveS1, bool& moveS2, bool& moveS3, Strategy* S1, Strategy* S2, Strategy* S3);
+	void GetGets(bool& moveS1, bool& moveS2, bool& moveS3, int& getS1, int& getS2, int& getS3);
+	void GetPoints(int& pointS1, int& pointS2, int& pointS3, int& getS1, int& getS2, int& getS3);
+	void PutRes(bool& moveS1, bool& moveS2, bool& moveS3, Strategy* S1, Strategy* S2, Strategy* S3);
+	void PrintRes(int& pointS1, int& pointS2, int& pointS3, Strategy* S1, Strategy* S2, Strategy* S3);
+	void SetWinners(int& pointS1, int& pointS2, int& pointS3, int& S1, int& S2, int& S3);
+	std::string GetName();
+	int pointS1, pointS2, pointS3;
+	std::string name;
+};
