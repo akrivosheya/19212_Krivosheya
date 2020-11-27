ef0a6470942f98f42d7ef43754c908bf78028275
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2020-11-27 23:54:49 +0700

    Start to connect slot

--- Lab3/window.cpp
+++ Lab3/window.cpp
@@ -1,7 +1,10 @@
 #include "window.h"
+#include <QGridLayout>
 
 Window::Window()
 {
+    game = new Game();
+    renderArea = new RenderArea();
     play_button = new QPushButton(tr("&Play"));
     stop_button = new QPushButton(tr("&Stop"));
     save_button = new QPushButton(tr("&Save"));
@@ -13,12 +16,21 @@ Window::Window()
     mainLayout->addWidget(save_button, 0, 2);
     mainLayout->addWidget(load_button, 0, 3);
     mainLayout->addWidget(mode, 0, 4);
+    mainLayout->addWidget(renderArea, 1, 0, 1, 5);
     setLayout(mainLayout);
     setWindowTitle(tr("The Life"));
+    connect(renderArea, &RenderArea::mousePressEvent,
+            this, &Window::changeRect);
+}
+
+void Window::changeRect(){
+    
 }
 
 Window::~Window()
 {
+    delete game;
+    delete renderArea;
     delete play_button;
     delete stop_button;
     delete save_button;
