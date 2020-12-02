399255d48aa8c751b7e0146e14e9a66d8ceb24cf
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2020-12-02 16:48:49 +0700

    Update Game.h

--- lab2/Game.h
+++ lab2/Game.h
@@ -6,18 +6,24 @@
 
 class Game {
 public:
+	Game();
+	virtual ~Game() {}
 	virtual void Play(int& steps, std::vector<Strategy*>& strategys, const char* file) = 0;
 	int GetPoint(int i);
-	virtual ~Game() {}
-protected:
-	std::string Moves(bool& moveS1, bool& moveS2, bool& moveS3);
-	void GetMoves(bool& moveS1, bool& moveS2, bool& moveS3, Strategy* S1, Strategy* S2, Strategy* S3);
-	void GetGets(bool& moveS1, bool& moveS2, bool& moveS3, int& getS1, int& getS2, int& getS3);
-	void GetPoints(int& pointS1, int& pointS2, int& pointS3, int& getS1, int& getS2, int& getS3);
-	void PutRes(bool& moveS1, bool& moveS2, bool& moveS3, Strategy* S1, Strategy* S2, Strategy* S3);
-	void PrintRes(int& pointS1, int& pointS2, int& pointS3, Strategy* S1, Strategy* S2, Strategy* S3);
-	void SetWinners(int& pointS1, int& pointS2, int& pointS3, int& S1, int& S2, int& S3);
 	std::string GetName();
+	
+	int countTournament;
+	bool moveSouth1, moveSouth2, moveSTFT1, moveBack, moveKind, moveTraitor, moveTK, moveTA;
+protected:
+	std::string Moves();
+	void GetMoves(Strategy* S1, Strategy* S2, Strategy* S3);
+	void GetGets();
+	void GetPoints();
+	void PutRes(Strategy* S1, Strategy* S2, Strategy* S3);
+	void PrintRes(Strategy* S1, Strategy* S2, Strategy* S3);
+	void SetWinners(int& S1, int& S2, int& S3);
+	bool moveS1, moveS2, moveS3;
+	int getS1, getS2, getS3;
 	int pointS1, pointS2, pointS3;
 	std::string name;
 };
