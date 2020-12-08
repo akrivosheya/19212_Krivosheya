1f736743f0c925d1c8a456a9d38cd3f1f4b1dcca
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2020-12-08 21:16:02 +0700

    Update Game.h

--- lab2/Game.h
+++ lab2/Game.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include "Strategy.h"
+#include "Printer.h"
 #include <vector>
 #include <fstream>
 
@@ -8,20 +9,20 @@ class Game {
 public:
 	Game();
 	virtual ~Game() {}
-	virtual void Play(int& steps, std::vector<Strategy*>& strategys, const char* file) = 0;
+	virtual void Play(const char* file, Printer& print){}
 	int GetPoint(int i);
 	std::string GetName();
-	
-	int countTournament;
-	bool moveSouth1, moveSouth2, moveSTFT1, moveBack, moveKind, moveTraitor, moveTK, moveTA;
+	void SetSteps(int& steps);
+	void SetStrategys(std::vector<Strategy*>& strategys);
 protected:
 	std::string Moves();
 	void GetMoves(Strategy* S1, Strategy* S2, Strategy* S3);
-	void GetGets();
+	void GetGets(const char* file);
 	void GetPoints();
 	void PutRes(Strategy* S1, Strategy* S2, Strategy* S3);
-	void PrintRes(Strategy* S1, Strategy* S2, Strategy* S3);
 	void SetWinners(int& S1, int& S2, int& S3);
+	int steps;
+	std::vector<Strategy*> strategys;
 	bool moveS1, moveS2, moveS3;
 	int getS1, getS2, getS3;
 	int pointS1, pointS2, pointS3;
