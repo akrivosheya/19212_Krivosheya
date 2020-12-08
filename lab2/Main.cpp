b8c94220db0eaa2201c82df4e26ce1044927591f
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2020-12-08 21:20:21 +0700

    Update Main.cpp
    Use unique_ptr
    Use Matrix
    Printer print results
    move, steps, strategys are inside Game

--- lab2/Main.cpp
+++ lab2/Main.cpp
@@ -3,16 +3,12 @@
 
 int main(int argc, char* argv[]) {
 	if (argc < 4) return -1;
-	int steps = 0;
-	std::string mode = "IsNotSet";
-	std::vector<Strategy*> strategys;
 	Preparation P;
-	Game* G = P.Prepare(steps, mode, strategys, argc, argv);
+	std::unique_ptr<Game> G(P.Prepare(argc, argv));
 	if (!G) {
 		return -1;
 	}
-	G->Play(steps, strategys, "Matrix.txt");
-	delete G;
+	Printer Print;
+	G->Play("Matrix.txt", Print);
 	return 0;
-
 }
