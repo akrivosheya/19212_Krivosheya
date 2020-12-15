9017105adaa7ae80f7e827f976e083d67e58221a
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2020-12-16 03:07:37 +0700

    Create Display.h

---
+++ lab2/Display.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Game.h"
+#include <vector>
+#include "Strategy.h"
+
+class Display {
+public:
+	int GetPoint(int i, Game* G);
+	int GetSteps(Game* G);
+	bool GetTraitor1(Game* G);
+	bool GetTraitor2(Game* G);
+	bool GetKind1(Game* G);
+	bool GetKind2(Game* G);
+	bool GetBackAndForth3(Game* G);
+	bool GetTitForTat1(Game* G);
+	int GetTournamentCount(Game* G);
+	std::vector<Strategy*>& GetStrategys(Game* G);
+	std::string GetName(Game* G);
+};
