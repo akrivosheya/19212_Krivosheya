164beffe0ebb9c7047dea39eb1f61bfde8c82f3b
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2020-12-16 03:27:31 +0700

    Update Game.h

--- Lab3/Game.h
+++ Lab3/Game.h
@@ -1,19 +1,37 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <QObject>
 #include <vector>
 
-class Game{
+class Game: public QObject{
+    Q_OBJECT
 public:
-    Game(int width = 25, int height = 25);
-    std::vector<std::vector<bool> >GetMatrix();
-    int GetHeight();
-    int GetWidth();
+    Game(int width = defaultSize, int height = defaultSize,
+         int rule1 = defaultRule1, int rule2 = defaultRule2, int rule3 = defaultRule3);
+    std::vector<std::vector<bool> >& GetMatrix();
+    void SetRules(QString);
+
+public slots:
+    void ChangeRect(int idx1, int idx2);
+    void Activate();
+    void Diactivate();
+    void Resize(int width, int height);
+
+signals:
+    void Changed();
+
 private:
+    void timerEvent(QTimerEvent *event) override;
+    static constexpr int defaultSize = 25;
+    static constexpr int defaultRule1 = 3;
+    static constexpr int defaultRule2 = 2;
+    static constexpr int defaultRule3 = 3;
+    static constexpr int rulesSize = 5;
     std::vector<std::vector<bool> > matrix;
-    int width;
-    int height;
-    bool play;
+    std::vector<int> rules;
+    int timerId;
+    bool play, haveTimerId;
 };
 
 #endif // GAME_H
