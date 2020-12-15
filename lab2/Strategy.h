4783c68bd6183effd4dd5b7da95813344a86c2bb
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2020-12-16 03:19:59 +0700

    Update Strategy.h

--- lab2/Strategy.h
+++ lab2/Strategy.h
@@ -4,11 +4,11 @@
 class Strategy {
 public:
 	virtual ~Strategy() {}
-	virtual void getRes(bool s1, bool s2) = 0;
-	virtual bool makeMove() = 0;
+	virtual void putDecisions(bool decision1, bool decision2) = 0;
+	virtual bool makeDecision() = 0;
 	virtual std::string getName() = 0;
 	virtual void reload() = 0;
 protected:
-	static constexpr bool c = true;
-	static constexpr bool d = false;
+	static constexpr bool help = true;
+	static constexpr bool betray = false;
 };
