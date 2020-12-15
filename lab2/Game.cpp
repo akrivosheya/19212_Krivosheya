71d30002c54105bf4955760ecc50845cf531d978
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2020-12-16 03:11:44 +0700

    Update Game.cpp

--- lab2/Game.cpp
+++ lab2/Game.cpp
@@ -2,63 +2,66 @@
 #include <fstream>
 
 Game::Game(): steps(0),
-			moveS1(false),
-			moveS2(false), 
-			moveS3(false), 
+			decisionS1(false),
+			decisionS2(false),
+			decisionS3(false),
 			getS1(0), 
 			getS2(0), 
 			getS3(0), 
 			pointS1(0), 
 			pointS2(0), 
-			pointS3(0){}
+			pointS3(0),
+			tournamentCount(0),
+			traitor1(true), 
+			traitor2(true),
+			kind1(true),
+			kind2(true),
+			backandforth3(true),
+			titfortat1(true){}
 
 Game::~Game() {
 	strategys.clear();
 }
 
-std::string Game::Moves() {
-	std::string m1 = ((moveS1) ? "C" : "D");
-	std::string m2 = ((moveS2) ? "C" : "D");
-	std::string m3 = ((moveS3) ? "C" : "D");
+std::string Game::Decisions() {
+	std::string m1 = ((decisionS1) ? "C" : "D");
+	std::string m2 = ((decisionS2) ? "C" : "D");
+	std::string m3 = ((decisionS3) ? "C" : "D");
 	std::string moves = " " + m1 + "  " + m2 + "  " + m3;
 	return moves;
 }
 
-void Game::GetMoves(Strategy* S1, Strategy* S2, Strategy* S3) {
-	moveS1 = S1->makeMove();
-	moveS2 = S2->makeMove();
-	moveS3 = S3->makeMove();
+void Game::SetDecisions(Strategy* S1, Strategy* S2, Strategy* S3) {
+	decisionS1 = S1->makeDecision();
+	decisionS2 = S2->makeDecision();
+	decisionS3 = S3->makeDecision();
 }
 
-void Game::GetGets(const char* file) {
+void Game::SetGets(const char* file) {
 	bool found = false;
 	char c1, c2, c3;
 	std::ifstream matrix(file);
 	while (!found) {
 		matrix >> c1 >> c2 >> c3 >> getS1 >> getS2 >> getS3;
-		if (((moveS1 && (c1 == 'c')) || (!moveS1 && (c1 == 'd')))
-			&& ((moveS2 && (c2 == 'c')) || (!moveS2 && (c2 == 'd')))
-			&& ((moveS3 && (c3 == 'c')) || (!moveS3 && (c3 == 'd')))) {
+		if (((decisionS1 && (c1 == 'c')) || (!decisionS1 && (c1 == 'd')))
+			&& ((decisionS2 && (c2 == 'c')) || (!decisionS2 && (c2 == 'd')))
+			&& ((decisionS3 && (c3 == 'c')) || (!decisionS3 && (c3 == 'd')))) {
 			found = true;
 		}
 	}
 	matrix.close();
 }
 
-std::string Game::GetName() {
-	return name;
-}
-
-void Game::GetPoints() {
+void Game::SetPoints() {
 	pointS1 += getS1;
 	pointS2 += getS2;
 	pointS3 += getS3;
 }
 
-void Game::PutRes(Strategy* S1, Strategy* S2, Strategy* S3) {
-	S1->getRes(moveS2, moveS3);
-	S2->getRes(moveS1, moveS3);
-	S3->getRes(moveS1, moveS2);
+void Game::GiveDecisions(Strategy* S1, Strategy* S2, Strategy* S3) {
+	S1->putDecisions(decisionS2, decisionS3);
+	S2->putDecisions(decisionS1, decisionS3);
+	S3->putDecisions(decisionS1, decisionS2);
 }
 
 void Game::SetWinners(int& S1, int& S2, int& S3) {
@@ -82,17 +85,6 @@ void Game::SetWinners(int& S1, int& S2, int& S3) {
 	S1 += (max1) ? 1 : 0;
 	S2 += (max2) ? 1 : 0;
 	S3 += (max3) ? 1 : 0;
-}
-
-int Game::GetPoint(int i) {
-	switch (i) {
-	case 1:
-		return pointS1;
-	case 2:
-		return pointS2;
-	case 3:
-		return pointS3;
-	}
 }
 
 void Game::SetSteps(int& steps) {
