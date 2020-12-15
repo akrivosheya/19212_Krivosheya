84366f762cab23029a724221dd7d8a8b77fdbda9
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2020-12-16 03:12:19 +0700

    Update Game.h

--- lab2/Game.h
+++ lab2/Game.h
@@ -10,21 +10,22 @@ public:
 	Game();
 	virtual ~Game();
 	virtual void Play(const char* file, Printer& print){}
-	int GetPoint(int i);
-	std::string GetName();
 	void SetSteps(int& steps);
 	void SetStrategys(std::vector<Strategy*>& strategys);
 protected:
-	std::string Moves();
-	void GetMoves(Strategy* S1, Strategy* S2, Strategy* S3);
-	void GetGets(const char* file);
-	void GetPoints();
-	void PutRes(Strategy* S1, Strategy* S2, Strategy* S3);
+	std::string Decisions();
+	void SetDecisions(Strategy* S1, Strategy* S2, Strategy* S3);
+	void SetGets(const char* file);
+	void SetPoints();
+	void GiveDecisions(Strategy* S1, Strategy* S2, Strategy* S3);
 	void SetWinners(int& S1, int& S2, int& S3);
 	int steps;
 	std::vector<Strategy*> strategys;
-	bool moveS1, moveS2, moveS3;
+	bool decisionS1, decisionS2, decisionS3;
 	int getS1, getS2, getS3;
 	int pointS1, pointS2, pointS3;
+	//для тестов
 	std::string name;
+	int tournamentCount;
+	bool traitor1, traitor2, kind1, kind2, backandforth3, titfortat1;
 };
