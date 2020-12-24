c44d46d11a309aea1d89cb085d1a91c40fd23c59
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2020-12-24 23:59:08 +0700

    Update DataMaster.h

--- Lab3/DataMaster.h
+++ Lab3/DataMaster.h
@@ -2,13 +2,22 @@
 #define DATAMASTER_H
 
 #include <QObject>
+#include <iostream>
 
 class DataMaster: public QObject{
     Q_OBJECT;
 
 public slots:
-    void Save(std::vector<std::vector<bool> >& matrix, std::vector<int>& rules);
-    void Load(std::vector<std::vector<bool> >& matrix, std::vector<int>& rules);
+    void Save(std::vector<std::vector<bool> >& matrix,
+              std::vector<int>& rules);
+    void Load(std::vector<std::vector<bool> >& matrix,
+              std::vector<int>& rules);
+
+private:
+    bool GetVector(QDataStream& in, std::vector<int>& someVector, int limit);
+    bool GetMatrix(QDataStream& in, std::vector<std::vector<bool> >& matrix);
+    static constexpr int maxSize = 100;
+    static constexpr int maxRule = 8;
 };
 
 #endif // DATAMASTER_H
