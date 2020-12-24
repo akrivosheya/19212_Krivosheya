c78700666d242fc883ce947a20eba996e7d784d1
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2020-12-24 23:52:44 +0700

    Update Tournament.h

--- lab2/Tournament.h
+++ lab2/Tournament.h
@@ -3,9 +3,11 @@
 class Tournament : public Game {
 public:
 	Tournament();
-	void Play(const char* file, Printer& print) override;
+	void Play(Printer& print) override;
 private:
 	void Reload(Strategy* S1, Strategy* S2, Strategy* S3);
 };
 
-Game* createTournament();
+Game* createTournament() {
+	return new Tournament();
+}
