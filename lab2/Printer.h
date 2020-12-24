b8c73d6fc3ab2d634cde893933e2a6b4ebd0c588
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2020-12-24 23:41:23 +0700

    Update Printer.h

--- lab2/Printer.h
+++ lab2/Printer.h
@@ -1,16 +1,18 @@
 #pragma once
-#include "Strategy.h"
 #include <vector>
+#include <iostream>
+
+#include "Strategy.h"
 
 class Printer {
 public:
 	Printer() = default;
-	void PrintRes(Strategy* S1, Strategy* S2, Strategy* S3, 
-		int pointS1, int pointS2, int pointS3);
+	void PrintRes(std::vector<Strategy*>& strategys, 
+		std::vector<int> points, int i, int j, int k);
 
-	void PrintTournamentRes(std::vector<Strategy*>& strategys, std::vector<int>& victories);
+	void PrintTournamentRes(std::vector<Strategy*>& strategys, 
+				std::vector<int>& victories);
 
-	void PrintDetailedRes(bool moveS1, bool moveS2, bool moveS3,
-		int pointS1, int pointS2, int pointS3,
-		int getS1, int getS2, int getS3);
+	void PrintDetailedRes(std::vector<bool> decisions, 
+			      std::vector<int> gets, std::vector<int> points);
  };
