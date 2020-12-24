b1d653b349ac1de1c0ae1ca8655b9860a6e321d3
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2020-12-24 23:29:52 +0700

    Update Detailed.cpp

--- lab2/Detailed.cpp
+++ lab2/Detailed.cpp
@@ -14,22 +14,16 @@ Detailed::Detailed(){
 	name = "Detailed";
 }
 
-void Detailed::Play(const char* file, Printer& print) {
+void Detailed::Play(Printer& print) {
 	std::string step;
 	std::cin >> step;
 	while (step != "quit") {
 		SetDecisions(strategys[0], strategys[1], strategys[2]);
-		SetGets(file);
+		SetGets();
 		SetPoints();
 		GiveDecisions(strategys[0], strategys[1], strategys[2]);
-		print.PrintDetailedRes(decisionS1, decisionS2, decisionS3,
-			pointS1, pointS2, pointS3,
-			getS1, getS2, getS3);
+		print.PrintDetailedRes(decisions, gets, points);
 		std::cin >> step;
 	}
-	print.PrintRes(strategys[0], strategys[1], strategys[2], pointS1, pointS2, pointS3);
-}
-
-Game* createDetailed() {
-	return new Detailed();
+	print.PrintRes(strategys, points, 0, 1, 2);
 }
