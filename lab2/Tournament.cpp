a843eea4607b12b088fbbb70d7f879f0bb5686f2
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2021-01-14 20:37:10 +0700

    Update Tournament.cpp

--- lab2/Tournament.cpp
+++ lab2/Tournament.cpp
@@ -1,6 +1,8 @@
 #include "Tournament.h"
 #include "Factory.h"
 
+Game* createTournament();
+
 namespace {
 	bool g() {
 		Factory<Game, std::string, Game* (*)()>::getInstance()->addCreator("tournament", createTournament);
@@ -10,10 +12,6 @@ namespace {
 	bool b = g();
 }
 
-Tournament::Tournament(){
-	name = "Tournament";
-}
-
 void Tournament::Play(Printer& print) {
 	int limit = strategys.size();
 	std::vector<int> victories(limit);
@@ -21,14 +19,14 @@ void Tournament::Play(Printer& print) {
 		for (int j = i + 1; j < limit; ++j) {
 			for (int k = j + 1; k < limit; ++k) {
 				for (int s = 0; s < steps; ++s) {
-					SetDecisions(strategys[i], strategys[j], strategys[k]);
+					SetDecisions(strategys[i].get(), strategys[j].get(), strategys[k].get());
 					SetGets();
 					SetPoints();
-					GiveDecisions(strategys[i], strategys[j], strategys[k]);
+					GiveDecisions(strategys[i].get(), strategys[j].get(), strategys[k].get());
 				}
 				SetWinners(victories[i], victories[j], victories[k]);
 				print.PrintRes(strategys, points, i, j, k);
-				Reload(strategys[i], strategys[j], strategys[k]);
+				Reload(strategys[i].get(), strategys[j].get(), strategys[k].get());
 				points.assign(3, 0);
 				++tournamentCountForTest;
 			}
@@ -41,4 +39,8 @@ void Tournament::Reload(Strategy* S1, Strategy* S2, Strategy* S3) {
 	S1->reload();
 	S2->reload();
 	S3->reload();
+}
+
+Game* createTournament() {
+	return new Tournament();
 }
