95b5a3af69bd931fae79c9396a129d3863ad9238
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2021-01-14 20:36:34 +0700

    Update TitForTatKind.h

--- lab2/TitForTatKind.h
+++ lab2/TitForTatKind.h
@@ -1,16 +1,19 @@
+#pragma once
 #include "Strategy.h"
 
 class TitForTatKind : public Strategy {
 public:
 	void putDecisions(bool decision1, bool decision2) override;
-	bool makeDecision() override;
-	std::string getName() override;
-	void reload() override;
+	bool makeDecision() override {
+		return betrayed;
+	}
+	std::string getName() override {
+		return name;
+	}
+	void reload() override {
+		betrayed = false;
+	}
 private:
 	bool betrayed = false;
 	std::string name = "TitForTatKind";
 };
-
-Strategy* createTitForTatKind() {
-	return new TitForTatKind;
-}
