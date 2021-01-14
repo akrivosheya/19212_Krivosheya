dc9d9a1e3c4f74f9993de8b96cdf4cc9e3effa8a
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2021-01-14 20:23:50 +0700

    Update Display.h

--- lab2/Display.h
+++ lab2/Display.h
@@ -6,15 +6,37 @@
 
 class Display {
 public:
-	std::vector<int>& GetPoint(Game* G);
-	int GetSteps(Game* G);
-	bool GetTraitor1(Game* G);
-	bool GetTraitor2(Game* G);
-	bool GetKind1(Game* G);
-	bool GetKind2(Game* G);
-	bool GetBackAndForth3(Game* G);
-	bool GetTitForTat1(Game* G);
-	int GetTournamentCount(Game* G);
-	std::vector<Strategy*>& GetStrategys(Game* G);
-	std::string GetName(Game* G);
+	std::vector<int>& GetPoint(Game* G) {
+		return G->points;
+	}
+	std::string GetName(Game* G) {
+		return G->name;
+	}
+	int GetSteps(Game* G) {
+		return G->steps;
+	}
+	std::vector<std::unique_ptr<Strategy> >& GetStrategys(Game* G) {
+		return G->strategys;
+	}
+	int GetTournamentCount(Game* G) {
+		return G->tournamentCountForTest;
+	}
+	bool GetTraitor1(Game* G) {
+		return G->traitor1ForTest;
+	}
+	bool GetTraitor2(Game* G) {
+		return G->traitor2ForTest;
+	}
+	bool GetKind1(Game* G) {
+		return G->kind1ForTest;
+	}
+	bool GetKind2(Game* G) {
+		return G->kind2ForTest;
+	}
+	bool GetBackAndForth3(Game* G) {
+		return G->backandforth3ForTest;
+	}
+	bool GetTitForTat1(Game* G) {
+		return G->titfortat1ForTest;
+	}
 };
