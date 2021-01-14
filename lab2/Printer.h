39b4b87deb86e118a446ac4a1ded93ffaf6a98e0
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2021-01-14 20:31:17 +0700

    Update Printer.h

--- lab2/Printer.h
+++ lab2/Printer.h
@@ -7,12 +7,15 @@
 class Printer {
 public:
 	Printer() = default;
-	void PrintRes(std::vector<Strategy*>& strategys, 
+	void PrintRes(std::vector<std::unique_ptr<Strategy> >& strategys, 
 		std::vector<int> points, int i, int j, int k);
 
-	void PrintTournamentRes(std::vector<Strategy*>& strategys, 
-				std::vector<int>& victories);
+	void PrintTournamentRes(std::vector<std::unique_ptr<Strategy> >& strategys,
+		std::vector<int>& victories);
 
-	void PrintDetailedRes(std::vector<bool> decisions, 
-			      std::vector<int> gets, std::vector<int> points);
+	void PrintDetailedRes(std::vector<bool> decisions, std::vector<int> gets, std::vector<int> points);
+
+	void ReadStep(std::string& step) {
+		std::cin >> step;
+	}
  };
