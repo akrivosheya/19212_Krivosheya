dbc7bde7389c8be66f9debe7e3744deeca6b130e
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2021-01-14 20:29:32 +0700

    Update Preparation.h

--- lab2/Preparation.h
+++ lab2/Preparation.h
@@ -6,7 +6,9 @@ class Preparation {
 public:
 	Game* Prepare(int& argc, char* argv[]);
 private:
-	bool IsMode(char* str);
+	bool IsMode(const std::string & str) {
+		return !(strcmp(str.c_str(), "detailed") && strcmp(str.c_str(), "fast") && strcmp(str.c_str(), "tournament"));
+	}
 	bool IsDigit(std::string str);
 	void SetModeOptional(int size, std::string& mode);
 	bool SetOptions(int size, std::string& mode, int& steps, int i, int argc, char* argv[]);
