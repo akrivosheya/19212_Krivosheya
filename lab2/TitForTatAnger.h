0d3ccfb3349b7546d8b012bad338ef021933ac8a
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2021-01-14 20:35:43 +0700

    Update TitForTatAnger.h

--- lab2/TitForTatAnger.h
+++ lab2/TitForTatAnger.h
@@ -4,12 +4,16 @@
 class TitForTatAnger : public Strategy {
 public:
 	void putDecisions(bool decision1, bool decision2) override;
-	bool makeDecision() override;
-	std::string getName() override;
-	void reload() override;
+	bool makeDecision() override {
+		return betrayed;
+	}
+	std::string getName() override {
+		return name;
+	}
+	void reload() override {
+		betrayed = false;
+	}
 private:
 	bool betrayed = false;
 	std::string name = "TitForTatAnger";
 };
-
-Strategy* createTitForTatAnger();
