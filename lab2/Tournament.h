14162b0994a49f31010b105871501019eeaccfb6
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2021-01-14 20:37:37 +0700

    Update Tournament.h

--- lab2/Tournament.h
+++ lab2/Tournament.h
@@ -1,13 +1,12 @@
+#pragma once
 #include "Game.h"
 
 class Tournament : public Game {
 public:
-	Tournament();
+	Tournament() {
+		name = "Tournament";
+	}
 	void Play(Printer& print) override;
 private:
 	void Reload(Strategy* S1, Strategy* S2, Strategy* S3);
 };
-
-Game* createTournament() {
-	return new Tournament();
-}
