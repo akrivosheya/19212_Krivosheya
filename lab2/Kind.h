2d91faeec19f85ddade57e78c27b17b4de479673
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2021-01-14 20:27:48 +0700

    Update Kind.h

--- lab2/Kind.h
+++ lab2/Kind.h
@@ -1,15 +1,18 @@
+#pragma once
 #include "Strategy.h"
 
 class Kind : public Strategy {
 public:
-	void putDecisions(bool decision1, bool decision2) override;
-	bool makeDecision() override;
-	std::string getName() override;
+	void putDecisions(bool decision1, bool decision2) override{
+		return;
+	}
+	bool makeDecision() override{
+		return help;
+	}
+	std::string getName() override{
+		return name;
+	}
 	void reload() override {};
 private:
 	std::string name = "Kind";
 };
-
-Strategy* createKind() {
-	return new Kind;
-}
