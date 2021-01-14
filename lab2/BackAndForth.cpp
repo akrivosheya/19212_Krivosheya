e38f96523d97cc6f5619f083e518c5ff26c8adca
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2021-01-14 20:21:41 +0700

    Update BackAndForth.cpp

--- lab2/BackAndForth.cpp
+++ lab2/BackAndForth.cpp
@@ -1,6 +1,8 @@
 #include "BackAndForth.h"
 #include "Factory.h"
 
+Strategy* createBackAndForth();
+
 namespace {
 	bool g() {
 		Factory<Strategy, std::string, Strategy* (*)()>::getInstance()->addCreator("backandforth", createBackAndForth);
@@ -10,22 +12,9 @@ namespace {
 	bool b = g();
 }
 
-void BackAndForth::putDecisions(bool decision1, bool decision2) {
-	return;
-}
-
 bool BackAndForth::makeDecision() {
 	cORd = !cORd;
 	return cORd;
-}
-
-
-std::string BackAndForth::getName() {
-	return name;
-}
-
-void BackAndForth::reload() {
-	cORd = help;
 }
 
 Strategy* createBackAndForth() {
