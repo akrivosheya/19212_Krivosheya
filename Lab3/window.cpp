0d5241845e81c48425a2c931d1ed767da8df48ec
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2021-01-16 20:15:56 +0700

    Update window.cpp

--- Lab3/window.cpp
+++ Lab3/window.cpp
@@ -7,7 +7,7 @@ Window::Window()
     game.reset(new Game());
     translater.reset(new Translater());
     dataMaster.reset(new DataMaster());
-    renderArea.reset(new RenderArea(game->GetMatrix()));
+    renderArea.reset(new RenderArea(game.get()));
     playButton.reset(new QPushButton(tr("&Play")));
     stopButton.reset(new QPushButton(tr("&Stop")));
     saveButton.reset(new QPushButton(tr("&Save")));
@@ -20,6 +20,8 @@ Window::Window()
     heightLine.reset(new QLineEdit());
     widthLabel.reset(new QLabel(tr("Width:"), nullptr));
     heightLabel.reset(new QLabel(tr("Height:"), nullptr));
+    informant.reset(new Informant());
+    timer.reset(new Timer());
 
     QGridLayout *mainLayout = new QGridLayout;
     mainLayout->addWidget(playButton.get(), 0, 0);
@@ -39,53 +41,141 @@ Window::Window()
     setWindowTitle(tr("The Life"));
 
     connect(renderArea.get(), &RenderArea::Clicked,
-            game.get(), &Game::ChangeRect);
-    connect(game.get(), &Game::Changed,
-            renderArea.get(), &RenderArea::Update);
+            this, &Window::ChangeRectOfMatrix);
     connect(playButton.get(), &QPushButton::pressed,
-            game.get(), &Game::Activate);
+            this, &Window::ActivateGame);
     connect(stopButton.get(), &QPushButton::pressed,
-            game.get(), &Game::Diactivate);
+            this, &Window::DiactivateGame);
     connect(clearButton.get(), &QPushButton::pressed,
-            game.get(), &Game::Clear);
+            this, &Window::ClearGame);
     connect(setRulesButton.get(), &QPushButton::pressed,
             this, &Window::SetRulesForGame);
     connect(setSizeButton.get(), &QPushButton::pressed,
             this, &Window::ResizeGame);
-    connect(translater.get(), &Translater::TranslatedRules,
-            game.get(), &Game::SetRules);
-    connect(translater.get(), &Translater::TranslatedSize,
-            game.get(), &Game::Resize);
     connect(saveButton.get(), &QPushButton::pressed,
             this, &Window::SaveGame);
     connect(loadButton.get(), &QPushButton::pressed,
             this, &Window::LoadGame);
+    connect(timer.get(), &Timer::TimeIsUp,
+            this, &Window::PlayGame);
 }
 
 void Window::SetRulesForGame(){
-    if(game->GetPlay()){
+    if(game->GetIsPlaying()){
         return;
     }
-    translater->TranslateRules(modeLine->text());
+    std::vector<std::vector<bool> > newRules;
+    if(!translater->TranslateRules(modeLine->text(),
+                                   newRules)){
+        informant->InformWrongRule();
+        return;
+    }
+    game->SetRules(newRules);
 }
 
 void Window::ResizeGame(){
-    if(game->GetPlay()){
+    if(game->GetIsPlaying()){
         return;
     }
-    translater->TranslateSize(widthLine->text(), heightLine->text());
+    std::vector<std::vector<bool>> newMatrix;
+    if(!translater->TranslateSize(widthLine->text(),
+                              heightLine->text(),
+                              newMatrix)){
+        informant->InformWrongSize();
+        return;
+    }
+    game->Resize(newMatrix);
+    renderArea->Update();
 }
 
 void Window::SaveGame(){
-    if(game->GetPlay()){
+    if(game->GetIsPlaying()){
         return;
     }
-    dataMaster->Save(game->GetMatrix(), game->GetRules());
+    QString error;
+    int res = dataMaster->Save(game->GetMatrix(),
+                               game->GetRules(),
+                               error);
+    if(res == dataMaster->GetSuccessCode()){
+        informant->InformSuccessfulSaving();
+        return;
+    }
+    if(res == dataMaster->GetEmptyCode()){
+        informant->InformEmpty();
+        return;
+    }
+    if(res == dataMaster->GetUnableCode()){
+        informant->InformUnable(error);
+        return;
+    }
+    if(res == dataMaster->GetFormatCode()){
+        informant->InformWrongFormat();
+        return;
+    }
 }
 
 void Window::LoadGame(){
-    if(game->GetPlay()){
+    if(game->GetIsPlaying()){
         return;
     }
-    dataMaster->Load(game->GetMatrix(), game->GetRules());
+    QString error;
+    int res = dataMaster->Load(game->GetMatrix(),
+                               game->GetRules(),
+                               error);
+    if(res == dataMaster->GetSuccessCode()){
+        informant->InformSuccessfulLoading();
+        return;
+    }
+    if(res == dataMaster->GetEmptyCode()){
+        informant->InformEmpty();
+        return;
+    }
+    if(res == dataMaster->GetUnableCode()){
+        informant->InformUnable(error);
+        return;
+    }
+    if(res == dataMaster->GetFormatCode()){
+        informant->InformWrongFormat();
+        return;
+    }
+}
+
+void Window::ActivateGame(){
+    if(game->GetIsPlaying()){
+        return;
+    }
+    game->Activate();
+    timer->StartTimer();
+}
+
+void Window::DiactivateGame(){
+    if(!game->GetIsPlaying()){
+        return;
+    }
+    game->Diactivate();
+    timer->KillTimer();
+}
+
+void Window::PlayGame(){
+    if(!game->GetIsPlaying()){
+        return;
+    }
+    game->MakeStep();
+    renderArea->Update();
+}
+
+void Window::ClearGame(){
+    if(game->GetIsPlaying()){
+        return;
+    }
+    game->Clear();
+    renderArea->Update();
+}
+
+void Window::ChangeRectOfMatrix(int idx1, int idx2){
+    if(game->GetIsPlaying()){
+        return;
+    }
+    game->ChangeRect(idx1, idx2);
+    renderArea->Update();
 }
