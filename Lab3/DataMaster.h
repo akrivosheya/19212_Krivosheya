68ddca19a6f63c510a0db4f8540afa43f07c189f
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2021-01-16 20:09:25 +0700

    Update DataMaster.h

--- Lab3/DataMaster.h
+++ Lab3/DataMaster.h
@@ -1,23 +1,35 @@
 #ifndef DATAMASTER_H
 #define DATAMASTER_H
 
-#include <QObject>
-#include <iostream>
+#include "LoaderAndSaver.h"
 
-class DataMaster: public QObject{
-    Q_OBJECT;
-
-public slots:
-    void Save(std::vector<std::vector<bool> >& matrix,
-              std::vector<int>& rules);
-    void Load(std::vector<std::vector<bool> >& matrix,
-              std::vector<int>& rules);
+class DataMaster{
+public:
+    int Save(std::vector<std::vector<bool> >& matrix,
+             std::vector<std::vector<bool> >& rules,
+             QString& error);
+    int Load(std::vector<std::vector<bool> >& matrix,
+             std::vector<std::vector<bool> >& rules,
+             QString& error);
+    int GetEmptyCode(){
+        return emptyName;
+    }
+    int GetUnableCode(){
+        return unableToOpen;
+    }
+    int GetFormatCode(){
+        return wrongFormat;
+    }
+    int GetSuccessCode(){
+        return success;
+    }
 
 private:
-    bool GetVector(QDataStream& in, std::vector<int>& someVector, int limit);
-    bool GetMatrix(QDataStream& in, std::vector<std::vector<bool> >& matrix);
-    static constexpr int maxSize = 100;
-    static constexpr int maxRule = 8;
+    static constexpr int success = 0;
+    static constexpr int emptyName = 1;
+    static constexpr int unableToOpen = 2;
+    static constexpr int wrongFormat = 3;
+    LoaderAndSaver helper;
 };
 
 #endif // DATAMASTER_H
