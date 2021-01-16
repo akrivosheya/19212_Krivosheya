ae7312d8310d080ff7f696f5ab0d9de6c439e2f9
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2021-01-16 20:13:53 +0700

    Update Game.cpp

--- Lab3/Game.cpp
+++ Lab3/Game.cpp
@@ -1,19 +1,18 @@
 #include <cctype>
 #include <cstdlib>
 #include <algorithm>
-#include <QMessageBox>
 
 #include "Game.h"
 
-Game::Game(int width, int height, int rule1, int rule2, int rule3):
-            matrix(height, std::vector<bool>(width)),
-            rules{rule1, rule2, rule3}{}
+Game::Game(int width, int height):
+           matrix(height, std::vector<bool>(width)),
+           rules(countOfRules, std::vector<bool>(maxNeighbours + 1)){
+    rules[0][defaultRuleOfLife] = true;
+    rules[1][defaultRuleOfDeathMin] = true;
+    rules[1][defaultRuleOfDeathMax] = true;
+}
 
-
-void Game::timerEvent(QTimerEvent *event){
-    if(!play){
-        return;
-    }
+void Game::MakeStep(){
     std::vector<std::vector<bool> > copy = matrix;
     for(unsigned int i = 0; i < matrix.size(); ++i){
         for(unsigned int j = 0; j < matrix[0].size(); ++j){
@@ -29,74 +28,28 @@ void Game::timerEvent(QTimerEvent *event){
                     count += matrix[i + k][j + l];
                 }
             }
-            if(matrix[i][j] && ((count < rules[1]) || (count > rules[2]))){
+            if(matrix[i][j] && !rules[1][count]){
                 copy[i][j] = false;
             }
-            else if(!matrix[i][j] && (count == rules[0])){
+            else if(!matrix[i][j] && rules[0][count]){
                 copy[i][j] = true;
             }
         }
     }
     matrix = copy;
-    emit Changed();
 }
-
-
-std::vector<std::vector<bool> >& Game::GetMatrix(){
-    return matrix;
-}
-
-void Game::SetRules(std::vector<int>& newRules){
-    rules = std::move(newRules);
-    emit Changed();
-}
-
-void Game::Resize(std::vector<std::vector<bool> >& newMatrix){
-    matrix = std::move(newMatrix);
-    emit Changed();
-}
-
 
 void Game::ChangeRect(int idx1, int idx2){
-    if(!play && (unsigned int)idx1 < matrix.size()
+    if(!isPlaying && (unsigned int)idx1 < matrix.size()
             && (unsigned int)idx2 < matrix[0].size()){
         matrix[idx1][idx2] = !matrix[idx1][idx2];
-        emit Changed();
     }
-}
-
-void Game::Activate(){
-    if(play){
-        return;
-    }
-    play = true;
-    timerId = startTimer(std::chrono::milliseconds(250));
-}
-
-void Game::Diactivate(){
-    if(!play){
-        return;
-    }
-    play = false;
-    killTimer(timerId);
 }
 
 void Game::Clear(){
-    if(play){
-        return;
-    }
     for(auto iter = matrix.begin(); iter != matrix.end(); ++iter){
         std::fill(iter->begin(), iter->end(), 0);
     }
-    emit Changed();
-}
-
-bool Game::GetPlay(){
-    return play;
-}
-
-std::vector<int>& Game::GetRules(){
-    return rules;
 }
 
 
