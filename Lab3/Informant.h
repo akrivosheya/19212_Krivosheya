440f7e6ace2159b90525db0d02c656c07552f756
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2021-01-16 20:10:32 +0700

    Create Informant.h

---
+++ Lab3/Informant.h
@@ -0,0 +1,40 @@
+#ifndef INFORMANT_H
+#define INFORMANT_H
+
+#include <QMessageBox>
+
+class Informant
+{
+public:
+    Informant() = default;
+    void InformWrongSize(){
+        QMessageBox::information(nullptr, "Error",
+                                "Wrong size");
+    }
+    void InformWrongRule(){
+        QMessageBox::information(nullptr, "Error",
+                                "Wrong rule");
+    }
+    void InformWrongFormat(){
+        QMessageBox::information(nullptr, "Error",
+                                "Wrong format");
+    }
+    void InformUnable(QString err){
+        QMessageBox::information(nullptr, "Error",
+                                err);
+    }
+    void InformEmpty(){
+        QMessageBox::information(nullptr, "Error",
+                                "Empty filename");
+    }
+    void InformSuccessfulSaving(){
+        QMessageBox::information(nullptr, "OK",
+                                "Successful saving");
+    }
+    void InformSuccessfulLoading(){
+        QMessageBox::information(nullptr, "OK",
+                                "Successful loading");
+    }
+};
+
+#endif // INFORMANT_H
