d2080dfcee4e4bbdbba110abd30d10dee3ad98b3
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2021-03-04 00:43:20 +0700

    Update Factory.java
    Use Properties. Check Exceptions

--- java/lab1/Factory.java
+++ java/lab1/Factory.java
@@ -1,71 +1,61 @@
 import java.util.*;
-import java.lang.Class;
-import java.io.InputStream;
+import java.lang.*;
+import java.io.*;
+import java.lang.reflect.InvocationTargetException;
 
 public class Factory {
 	private Factory(){
 		commands = new HashMap<>();
-		names = new HashMap<>();
+		names = new Properties();
 	}
 
-	public Object getObject(char symb) throws Exception{
-		if(!names.containsKey(symb)){
-			System.out.println("Wrong command: " + symb);
-			return null;
+	public Object getCommand(String key) {
+		String name = null;
+		if(commands.containsKey(key)){
+			return commands.get(key);
 		}
-		if(commands.containsKey(names.get(symb))){
-			return commands.get(names.get(symb));
+		if((name = names.getProperty(key)) == null){
+			return null;
 		}
 		Class<?> cl = null;
 		Object comm = null;
-			try{
-				cl = Class.forName(names.get(symb));
-				comm = cl.getDeclaredConstructor().newInstance();
-			} catch (ClassNotFoundException error){
-				System.err.println("Can't find class " + names.get(symb));
-				return null;
-			}
-		commands.put(names.get(symb), cl);
+
+		try{
+			cl = Class.forName(name);
+		} catch (ClassNotFoundException error){
+			throw new RuntimeException("Can't find class" + name);
+		}
+
+		try {
+			comm = cl.getDeclaredConstructor().newInstance();
+		} catch (NoSuchMethodException | InstantiationException |
+				IllegalAccessException | InvocationTargetException error){
+			throw new RuntimeException("Can't create an instance of the class" + name);
+		}
+
+		commands.put(key, cl);
 		return comm;
 	}
 
-	public Factory getInstance(){
+	public static Factory getInstance(){
 		if(instance == null){
 			instance = new Factory();
 		}
 		return instance;
 	}
 
-	public boolean configurate(InputStream reader) throws Exception{
-		String line = null;
-		char command;
-		int codeOfSymb = 0;
-		while(codeOfSymb != EOF){
-			line = "";
-			codeOfSymb = reader.read();
-			if(codeOfSymb == EOF){
-				return false;
-			}
-			command = (char)codeOfSymb;
-			codeOfSymb = reader.read();
-			if(codeOfSymb == EOF || codeOfSymb != '='){
-				return false;
-			}
-			codeOfSymb = reader.read();
-			while(codeOfSymb != EOF && codeOfSymb != '\n'){
-				line += (char)codeOfSymb;
-			}
-			if(line.length() < minLength){
-				return false;
-			}
-			names.put(command, line);
+	public boolean configurate(InputStream reader) {
+		try {
+			names.load(reader);
+		} catch (IOException error) {
+			return false;
 		}
 		return true;
 	}
 
 	static Factory instance = null;
-	private HashMap<String, Class<?>> commands;
-	private HashMap<Character, String>  names;
-	private static int EOF = -1;
-	private static int minLength = 1;
+	private Map<String, Class<?>> commands;
+	Properties names;
+	private int EOF = -1;
+	private int minLength = 1;
 };
