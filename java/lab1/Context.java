1f444f68b03346f23d6405026b3face86f91416b
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2021-03-08 18:51:00 +0700

    Update Context.java

--- java/lab1/Context.java
+++ java/lab1/Context.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.lang.RuntimeException;
+import java.io.*;
 
 public class Context{
 	Context(Scanner scan){
@@ -9,14 +10,10 @@ public class Context{
 			throw error;
 		}
 		stack = new ArrayDeque<Integer>();
-	}
-
-	public void printlnNumb(int num){
-		System.out.println(num);
-	}
-	
-	public void printlnString(String text){
-		System.out.println(text);
+		Writer ow = new OutputStreamWriter(System.out);
+		writer = new PrintWriter(ow, true);
+		Reader ir = new InputStreamReader(System.in);
+		reader = new BufferedReader(ir);
 	}
 
 	public char getKey(){
@@ -24,11 +21,17 @@ public class Context{
 	}
 	
 	public int get(){
+		if(stack.size() == 0){
+			return 0;
+		}
 		return stack.getLast();
 	}
 	
-	public int pop(){
-		return stack.removeLast();
+	public void pop(){
+		if(stack.size() == 0){
+			return;
+		}
+		stack.removeLast();
 	}
 	
 	public void push(int num){
@@ -42,8 +45,50 @@ public class Context{
 	public boolean jobIsFinished(){
 		return isFinished;
 	}
+
+	public int size(){
+		return stack.size();
+	}
+
+	public void changePosition(){
+		field.changePosition();
+	}
+
+	public void changeWayUp(){
+		field.changeWayUp();
+	}
+
+	public void changeWayDown(){
+		field.changeWayDown();
+	}
+
+	public void changeWayLeft(){
+		field.changeWayLeft();
+	}
+
+	public void changeWayRight(){
+		field.changeWayRight();
+	}
+
+	public BufferedReader getReader(){
+		return reader;
+	}
+
+	public PrintWriter getWriter(){
+		return writer;
+	}
+
+	public char getKey(int x, int y){
+		return field.getKey(x, y);
+	}
+
+	public void changeField(char value, int x, int y){
+		field.changeField(value, x, y);
+	}
 	
 	private Field field;
 	private ArrayDeque<Integer> stack;
 	private boolean isFinished = false;
+	private PrintWriter writer;
+	private BufferedReader reader;
 }
