f2b89ab18849e4ea7ff37f91b2ac94f9c2b24e0f
akrivosheya <70935529+akrivosheya@users.noreply.github.com>
2021-03-19 02:41:20 +0700

    Update Main.java

--- java/lab1/Main.java
+++ java/lab1/Main.java
@@ -7,10 +7,6 @@ public class Main{
 			return;
 		}
 		Befunge bef = new Befunge();
-		StringBuilder string = new StringBuilder("");
-		String[] buf = {"4", "6"};
-		bef.interpret(argv, new ContextIOForTest(string, buf));
-		//bef.interpret(argv, new ContextIO());
-		System.out.printf(string.substring(0));
+		bef.interpret(argv, new ContextIO());
 	}
 }
